46ed6b6b10f6d40ded2e322db68c0be0397ea922
Kacper Weiss <kacper.weiss@gmail.com>
2018-03-18 20:20:25 +0100

    destructor added

--- prj/inc/node.hpp
+++ prj/inc/node.hpp
@@ -22,7 +22,13 @@ class Node{
 
     public:
         // Public constructor for creating new nodes
-        Node(T value): data(value) {}
+        Node(T value): data(value), left(NULL), right(NULL), height(0) {}
+
+        // To avoid memory leaks, destructor deletes object's left and right nodes
+        ~Node(){
+            delete left;
+            delete right;
+        }
 };
 
 #endif
