c9c1e9b0df22664d7439858a50a06c5c2048865d
Kacper Weiss <kacper.weiss@gmail.com>
2018-03-19 17:46:48 +0100

    Test of AVL_Tree

--- prj/main.cpp
+++ prj/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include "inc/node.hpp"
 #include "inc/AVL_Tree.hpp"
+
+using namespace std;
 
 void showMenu(){
 
@@ -12,10 +16,15 @@ int main(){
 
     AVL_Tree<int> avlTreeOfIntegers;
 
+    srand(time(0));
+    int randomisedInput;
+
     for(int i = 0; i < 100; i++){
 
-        if(!avlTreeOfIntegers.insert(i))
-            std::cout << "Error while inserting value:" << i << std::endl;
+        randomisedInput = (rand() % 100) + 1;
+
+        if(!avlTreeOfIntegers.insert(randomisedInput))
+            cout << "Error while inserting value: " << randomisedInput << endl;
 
     }
 
